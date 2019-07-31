04c612cc46f5304d19a6b04890bbb45be8592186
Taras Terekh <taras.terekh@gmail.com>
2019-07-31 15:31:49 +0300

    Lecture 6

---
+++ Lecture_6/Lecture_6/Matrix.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_6
+{
+    class Matrix
+    {
+        private readonly int _rows;
+        private readonly int _columns;
+
+        private int[,] _matrix;
+
+        public Matrix (int rows, int columns)
+        {
+            this._rows = rows;
+            this._columns = columns;
+            _matrix = new int[_rows,_columns];
+        }
+
+
+        public int this[int index, int index1]
+        {
+            get => _matrix[index, index1];
+            set => _matrix[index, index1] = value;
+        }
+        
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix rezult = new Matrix(matrix1._rows, matrix1._columns);
+            for (int i = 0; i < matrix1._rows; i++)
+            {
+                for (int j = 0; j < matrix1._columns; j++)
+                {
+                    int sum = matrix1._matrix[i, j] + matrix2._matrix[i, j];
+                    rezult._matrix[i, j] = sum;
+                }
+            }
+            return rezult;
+        }
+
+        public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix rezult = new Matrix(matrix1._rows, matrix1._columns);
+            for (int i = 0; i < matrix1._rows; i++)
+            {
+                for (int j = 0; j < matrix1._columns; j++)
+                {
+                    int sum = matrix1._matrix[i, j] - matrix2._matrix[i, j];
+                    rezult._matrix[i, j] = sum;
+                }
+            }
+            return rezult;
+        }
+        
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix rezult = new Matrix(matrix1._rows, matrix1._columns);
+            for (int i = 0; i < matrix1._rows; i++)
+            {
+                for (int j = 0; j < matrix1._columns; j++)
+                {
+                    int sum = matrix1._matrix[i, j] * matrix2._matrix[j, i];
+                    rezult._matrix[i, j] = sum;
+                }
+            }
+            return rezult;
+        }
+
+        public void ToString()
+       {
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++)
+                {
+                    Console.Write(_matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+       }
+    }
+}
---
+++ Lecture_6/Lecture_6/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the number of rows  of the first matrix: ");
+            int rows1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the number of columns of the first matrix: ");
+            int columns1 = Convert.ToInt32(Console.ReadLine());
+            //int[,] matrix1 = new int [rows1, columns1];
+            Matrix matrix1 = new Matrix(rows1,columns1);
+            Console.WriteLine("\nMatrix 1:");
+            Random rand = new Random();
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < columns1; j++)
+                {
+                    matrix1[i,j] = rand.Next(0, 99);
+                    Console.Write("{0:D2} ", matrix1[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.Write("enter the number of rows  of the second matrix: ");
+            int rows2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the number of columns of the second matrix: ");
+            int columns2 = Convert.ToInt32(Console.ReadLine());
+            Matrix matrix2 = new Matrix(rows2, columns2);
+            Console.WriteLine("\nMatrix 2:");
+            for (int i = 0; i < rows2; i++)
+            {
+                for (int j = 0; j < columns2; j++)
+                {
+                    matrix2[i, j] = rand.Next(0, 99);
+                    Console.Write("{0:D2} ", matrix2[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("select action: Addition=1 Subtraction=2 Multiplication=3 \n");
+            Console.WriteLine("Rezult: ");
+            int select = Convert.ToInt32(Console.ReadLine());
+            Matrix matrix3 = new Matrix(1, 1);
+            Matrix matrix4 = new Matrix(1, 1);
+            Matrix matrix5 = new Matrix(1, 1);
+            switch (select)
+            {
+                case 1:
+                    try
+                    {
+                        matrix3 = matrix1 + matrix2;
+                        matrix3.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Exception: {ex.Message}");
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+                        matrix4 = matrix1 - matrix2;
+                        matrix4.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Exception: {ex.Message}");
+                    }
+                    break;
+                case 3:
+                    try
+                    {
+                        matrix5 = matrix1 * matrix2;
+                        matrix5.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Exception: {ex.Message}");
+                    }
+                    break;
+            }
+        }
+    }
+}
