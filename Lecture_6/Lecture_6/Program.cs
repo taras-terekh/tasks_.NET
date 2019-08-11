337cfe3bb1cf1e87449fc9b3fd85e5c6fd58eecb
Taras Terekh <taras.terekh@gmail.com>
2019-08-11 16:38:37 +0300

    Lecture 6

--- Lecture_6/Lecture_6/Program.cs
+++ Lecture_6/Lecture_6/Program.cs
@@ -10,44 +10,32 @@ namespace Lecture_6
     {
         static void Main(string[] args)
         {
-            Console.Write("enter the number of rows  of the first matrix: ");
-            int rows1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("enter the number of columns of the first matrix: ");
-            int columns1 = Convert.ToInt32(Console.ReadLine());
-            //int[,] matrix1 = new int [rows1, columns1];
-            Matrix matrix1 = new Matrix(rows1,columns1);
-            Console.WriteLine("\nMatrix 1:");
+            int rows1 = 4;
+            int columns1 = 4;
+            Matrix matrix1 = new Matrix(rows1, columns1);
             Random rand = new Random();
             for (int i = 0; i < rows1; i++)
             {
                 for (int j = 0; j < columns1; j++)
                 {
-                    matrix1[i,j] = rand.Next(0, 99);
-                    Console.Write("{0:D2} ", matrix1[i,j]);
+                    matrix1[i, j] = rand.Next(0, 99);
                 }
-                Console.WriteLine();
             }
-            Console.WriteLine();
-
-            Console.Write("enter the number of rows  of the second matrix: ");
-            int rows2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("enter the number of columns of the second matrix: ");
-            int columns2 = Convert.ToInt32(Console.ReadLine());
+            matrix1.ToString();
+            int rows2 = 4;
+            int columns2 = 4;
             Matrix matrix2 = new Matrix(rows2, columns2);
-            Console.WriteLine("\nMatrix 2:");
             for (int i = 0; i < rows2; i++)
             {
                 for (int j = 0; j < columns2; j++)
                 {
                     matrix2[i, j] = rand.Next(0, 99);
-                    Console.Write("{0:D2} ", matrix2[i, j]);
                 }
-                Console.WriteLine();
             }
+            matrix2.ToString();
             Console.WriteLine();
-            Console.WriteLine("select action: Addition=1 Subtraction=2 Multiplication=3 \n");
+            int select = 1;//Addition=1 Subtraction=2 Multiplication=3
             Console.WriteLine("Rezult: ");
-            int select = Convert.ToInt32(Console.ReadLine());
             Matrix matrix3 = new Matrix(1, 1);
             Matrix matrix4 = new Matrix(1, 1);
             Matrix matrix5 = new Matrix(1, 1);
